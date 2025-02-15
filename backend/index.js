9fe7cea6b082cfdc60e7dcfd0c52cbfa9ad4d32b
Ishant008 <tyagi8459800640@gmail.com>
2025-02-15 22:36:40 +0530

    update

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,8 @@ import mongoose from 'mongoose';
 import userRoutes from './App/routes/userRoutes.js';
 import noteRoutes from './App/routes/noteRoutes.js';
 import cors from "cors"
+import dotenv from "dotenv";
+dotenv.config();
 
 const app = express()
 app.use(express.json())
@@ -11,11 +13,13 @@ app.use(cors())
 app.use("/user",userRoutes)
 app.use("/notes",noteRoutes)
 
-mongoose.connect("mongodb://localhost:27017/notes_app")
+mongoose.connect(process.env.DB_URL)
 .then(()=>{
   console.log("db is connected");
+
+  let port = process.env.PORT || 8000
   
-  app.listen(8000,()=>{
+  app.listen(port,()=>{
     console.log("server is started");
   })
 })
