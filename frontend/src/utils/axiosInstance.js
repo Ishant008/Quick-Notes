09efa90cbff8d82603db92b7faf768f02e8caa62
Ishant008 <tyagi8459800640@gmail.com>
2025-02-16 12:58:57 +0530

    Update axiosInstance.js

--- frontend/src/utils/axiosInstance.js
+++ frontend/src/utils/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL:"http://localhost:8000"
+  baseURL:"https://quick-notes-backend-55yo.onrender.com"
 })
 
 axiosInstance.interceptors.request.use(
